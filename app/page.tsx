9ad41a5542390d8384f89c119228b2cfa0cc0e16
KumarRohit1423 <kumar.rohit.142001@gmail.com>
2023-11-17 03:38:50 +0530

    fix before sleep

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,17 @@
 import { roboto } from "@/app/ui/fonts";
 import { Button } from "@/app/ui/button";
+import Link from "next/link";
+
 export default async function Page() {
 	return (
-		<main className="bg-slate-950">
-			<div className="container my-24 mx-auto md:px-6">
-				<section className="mb-32">
-					<div className="relative overflow-hidden bg-cover bg-no-repeat bg-[50%] bg-[url('https://mdbcdn.b-cdn.net/img/new/textures/full/154.jpg')] h-[500px]"></div>
-					<div className="container mx-auto px-6 md:px-12 xl:px-32">
-						<div className="text-center">
-							<div className="mt-[-170px] block rounded-lg bg-[hsla(0,0%,100%,0.7)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] backdrop-blur-[30px] dark:bg-[hsla(0,0%,5%,0.55)] dark:shadow-black/20 md:py-16 md:px-12">
-								<h1 className="mb-16 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
-									The best platform
-									<br />
-									<span className="text-danger dark:text-danger-400">
-										for your Events
-									</span>
-								</h1>
-								{/* <a
-									className="mb-2 inline-block rounded bg-danger px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(220,76,100,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] md:mr-2 md:mb-0"
-									data-te-ripple-init
-									data-te-ripple-color="light"
-									href="#!"
-									role="button"
-								>
-									Get started
-								</a> */}
-								{/* <a
-									className="inline-block rounded px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:text-danger-700 dark:hover:bg-neutral-700 dark:hover:bg-opacity-40"
-									data-te-ripple-init
-									data-te-ripple-color="light"
-									href="#!"
-									role="button"
-								>
-									Learn more
-								</a> */}
-								<Button
-									className="inline-block"
-									children="Organizer Signup"
-								/>
-								<Button
-									className="inline-block"
-									children="Learn More"
-								/>
-							</div>
-						</div>
-					</div>
-				</section>
+		<main className="flex min-h-screen flex-col p-6 items-center justify-center">
+			<div className="flex items-center gap-8">
+				<h1 className={`${roboto.className} text-2xl antialiased`}>
+					Main Page
+				</h1>
+				<Link href={"/landing"}>
+					<Button children="Landing" />
+				</Link>
 			</div>
 		</main>
 	);
